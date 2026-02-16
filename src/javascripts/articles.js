6fcd43b1b6ac63f3dfd271b7ba97aa5d25742703
lunyc <lunyco@mail.ru>
2026-02-17 01:12:21 +0300

    test articles

--- src/javascripts/articles.js
+++ src/javascripts/articles.js
@@ -1,12 +1,12 @@
-import React from "react";
-import ArticlesSection from "./react/ArticlesSection.jsx";
-import { createRoot } from "react-dom/client";
+import React from 'react'
+import ArticlesSection from './react/ArticlesSection.jsx'
+import { createRoot } from 'react-dom/client'
 
-document.addEventListener("DOMContentLoaded", () => {
-  const container = document.getElementById("articles-cards-root");
+document.addEventListener('DOMContentLoaded', () => {
+  const container = document.getElementById('articles-cards-root')
 
-  if (!container) return;
+  if (!container) return
 
-  const root = createRoot(container);
-  root.render(<ArticlesSection />);
-});
+  const root = createRoot(container)
+  root.render(<ArticlesSection />)
+})
