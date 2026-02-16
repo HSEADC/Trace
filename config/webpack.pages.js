708de12fa56d39157188efb7b47a51183ff4c957
lunyc <lunyco@mail.ru>
2026-02-16 20:37:31 +0300

    ссылки хедера

--- config/webpack.pages.js
+++ config/webpack.pages.js
@@ -8,25 +8,34 @@ function createPages(template, filename) {
 }
 
 const htmlPages = [
-  createPages('./src/index.html', './index.html'),
-  createPages('./src/pages/style-guide.html', './pages/style-guide.html'),
-  createPages('./src/pages/articles.html', './pages/articles.html'),
-  createPages('./src/pages/game.html', './pages/game.html'),
+  createPages("./src/index.html", "./index.html"),
+  createPages("./src/pages/style-guide.html", "./pages/style-guide.html"),
+  createPages("./src/pages/articles.html", "./pages/articles.html"),
+  createPages("./src/pages/learn.html", "./pages/learn.html"),
+  createPages("./src/pages/game.html", "./pages/game.html"),
   createPages(
     './src/pages/articals/artical-degree.html',
     './pages/articals/artical-degree.html'
+    "./src/pages/articals/artical-degree.html",
+    "./pages/articals/artical-degree.html",
   ),
   createPages(
     './src/pages/articals/artical-cup.html',
     './pages/articals/artical-cup.html'
+    "./src/pages/articals/artical-cup.html",
+    "./pages/articals/artical-cup.html",
   ),
   createPages(
     './src/pages/articals/artical-razer.html',
     './pages/articals/artical-razer.html'
+    "./src/pages/articals/artical-razer.html",
+    "./pages/articals/artical-razer.html",
   ),
   createPages(
     './src/pages/articals/artical-liquid-or-solid.html',
     './pages/articals/artical-liquid-or-solid.html'
+    "./src/pages/articals/artical-liquid-or-solid.html",
+    "./pages/articals/artical-liquid-or-solid.html",
   ),
 
   createPages('./src/pages/theory.html', './pages/theory.html', ['theory']),
