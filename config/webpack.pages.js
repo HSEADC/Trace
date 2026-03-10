d84762c374694289cf84492c796d467eebfa7bcd
lunyc <lunyco@mail.ru>
2026-03-10 12:36:59 +0300

    chunks

--- config/webpack.pages.js
+++ config/webpack.pages.js
@@ -1,34 +1,41 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-function createPages(template, filename) {
+function createPages(template, filename, chunks) {
   return new HtmlWebpackPlugin({
     template: template,
     filename: filename,
+    chunks: chunks,
   });
 }
 
 const htmlPages = [
-  createPages("./src/index.html", "./index.html"),
-  createPages("./src/pages/style-guide.html", "./pages/style-guide.html"),
-  createPages("./src/pages/articles.html", "./pages/articles.html"),
-  createPages("./src/pages/learn.html", "./pages/learn.html"),
-  createPages("./src/pages/game.html", "./pages/game.html"),
-  createPages("./src/pages/about-us.html", "./pages/about-us.html"),
+  createPages("./src/index.html", "./index.html", ["index"]),
+  createPages("./src/pages/style-guide.html", "./pages/style-guide.html", [
+    "index",
+  ]),
+  createPages("./src/pages/articles.html", "./pages/articles.html", ["index"]),
+  createPages("./src/pages/learn.html", "./pages/learn.html", ["index"]),
+  createPages("./src/pages/game.html", "./pages/game.html", ["index"]),
+  createPages("./src/pages/about-us.html", "./pages/about-us.html", ["index"]),
   createPages(
     "./src/pages/articals/artical-degree.html",
     "./pages/articals/artical-degree.html",
+    ["index"],
   ),
   createPages(
     "./src/pages/articals/artical-cup.html",
     "./pages/articals/artical-cup.html",
+    ["index"],
   ),
   createPages(
     "./src/pages/articals/artical-razer.html",
     "./pages/articals/artical-razer.html",
+    ["index"],
   ),
   createPages(
     "./src/pages/articals/artical-liquid-or-solid.html",
     "./pages/articals/artical-liquid-or-solid.html",
+    ["index"],
   ),
 ];
 
